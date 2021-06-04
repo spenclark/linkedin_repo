eb47a4b628b64b3f0defee9ff30630b041e22489
Spencer <spenclark@hotmail.com>
2021-06-03 17:46:12 -0700

    route base and login component init

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App">LinkedIn UI</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
