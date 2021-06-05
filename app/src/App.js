bf79d4333289b67d460d2727b20020b838c7a07f
Spencer <spenclark@hotmail.com>
2021-06-04 19:34:29 -0700

    styles for header

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
 import "./App.css";
 import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import Header from "./components/Home/Header";
 
 function App() {
   return (
@@ -10,6 +12,10 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Login />
+          </Route>
+          <Route path="/home">
+            <Header />
+            <Home />
           </Route>
         </Switch>
       </Router>
--- app/src/components/Home/Header.jsx
+++ app/src/components/Home/Header.jsx
@@ -1,2 +1,14 @@
 import React from "react"
-import styled from "styled-components"
+import {HeaderContainer, HeaderContent} from "./HomeStyledComponents"
+
+const Header = props => {
+    return  (
+        <HeaderContainer>
+            <HeaderContent>
+
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
+
+export default Header
