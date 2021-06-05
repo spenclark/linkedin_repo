cbb05cee637779a7796df99d2357f1667096dcb9
Spencer <spenclark@hotmail.com>
2021-06-04 19:56:44 -0700

    Header Nav build out

--- app/src/components/Home/Header.jsx
+++ app/src/components/Home/Header.jsx
@@ -1,11 +1,34 @@
 import React from "react"
-import {HeaderContainer, HeaderContent} from "./HomeStyledComponents"
+import {HeaderContainer, HeaderContent, HeaderLogo, HeaderSearch, HeaderSearchIcon, HeaderNav, HeaderNavListWrap, HeaderNavList} from "./HomeStyledComponents"
 
 const Header = props => {
     return  (
         <HeaderContainer>
             <HeaderContent>
-
+                <HeaderLogo>
+                    <a href="/home">
+                        <img src="/assets/home-logo.svg"></img>
+                    </a>
+                </HeaderLogo>
+                <HeaderSearch>
+                    <HeaderSearchIcon>
+                        <img src="/assets/search-icon.svg"></img>
+                    </HeaderSearchIcon>         
+                    {/* Search Box (not functional) */}
+                    <div>
+                        <input type="text" placeholder="Search"/>
+                    </div>
+                   
+                </HeaderSearch>
+               <HeaderNav>
+                   <HeaderNavListWrap>
+                       <HeaderNavList>
+                           <a href="">
+                               <img src="/assets/nav-home.svg"> </img>
+                           </a>
+                       </HeaderNavList>
+                   </HeaderNavListWrap>
+               </HeaderNav>
             </HeaderContent>
         </HeaderContainer>
     )
